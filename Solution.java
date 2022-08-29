d3504ed03c94816359e371361751f9a90081bce8
csrgrr <104082439+csrgrr@users.noreply.github.com>
2022-08-29 13:28:19 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -30,20 +30,48 @@ public class Solution {
 					}
 					
 				} else if (caracteres[i] == 'S') {
-					if(contador > i) {
+					if(contador >= i) {
 						tiempoContado = tiempoContado + 40;
 					} else {
 						tiempoContado = tiempoContado + 30;
 					}
 				}
 				}
+	    
 			//si es menor, pero mayor que 0, substutuir.	
 	    	if(tiempoContado < tiempo || tiempo == 0) {
 	    		tiempo = tiempoContado;
 	    	}
 	    	contador++;
 	    }
-	    	
+		    
+		    //control de errores (todo el tiempo con y sin el scooter
+	    	tiempoContado=0;
+			for (int i = 0; i < caracteres.length; i++) {
+    	    	
+				if(caracteres[i] == 'A') {
+						tiempoContado = tiempoContado + 20;	
+				} else if (caracteres[i] == 'S') {
+						tiempoContado = tiempoContado + 30;
+				}
+				}
+			if(tiempoContado < tiempo || tiempo == 0) {
+	    		tiempo = tiempoContado;
+	    	}
+			
+	    	tiempoContado=0;
+			for (int i = 0; i < caracteres.length; i++) {
+    	    	
+				if(caracteres[i] == 'A') {
+						tiempoContado = tiempoContado + 5;	
+				} else if (caracteres[i] == 'S') {
+						tiempoContado = tiempoContado + 40;
+				}
+				}
+			if(tiempoContado < tiempo || tiempo == 0) {
+	    		tiempo = tiempoContado;
+	    	}
+			
 	    	
 	    	return tiempo;
 	    }
